d4bf8d0c375f46a5d162d3c17a0838529d7e46b6
RoigRosa201 <roigrosa88@gmail.com>
2019-03-09 02:53:41 -0500

    added article params and form for title/description

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,3 +7,20 @@ def new
 end
 
 end
+
+def create
+
+    #render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_show(@article)
+end
+
+private
+
+def article_params
+
+    params.require(:article).permit(:title, :description)
+
+    end
+
