f46c9395c901f3ed627b87a639eaae38cf1388e8
RoigRosa201 <roigrosa88@gmail.com>
2019-03-19 03:11:14 -0400

    add user validations

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+    validates :username, presence: true, 
+        uniqueness: { case_sensitive: false }, 
+        length: { minimum: 3, maximum: 25}
+    VALID_EMAIL_REGEX = /(?<month>\d{1,2})\/(?<day>\d{1,2})\/(?<year>\d{4})/i
+    validates :email, presence: true, length: { maximum: 100},
+        uniqueness: { case_sensitive: false}, 
+        format: { with: VALID_EMAIL_REGEX }
+end
